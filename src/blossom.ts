8bd32cdaf6205e179e42b414228d3488ad2d77d5
Biz <chebizarro@gmail.com>
2026-01-04 22:33:17 -0800

    chore: add utils module export and remove obsolete blossom re-export
    Add utils module to package exports to enable direct imports. Remove redundant blossom.ts re-export file since blossom module is already exported in package.json.
    
    Changes:
    - Add utils export in package.json exports field with types and default paths
    - Delete src/blossom.ts re-export file (already exported via package.json)

--- src/blossom.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './blossom/blossom.js';
-export * from './blossom/blossom.js';
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export * from "./canonicalRepoKey.js";
+export * from "./nip05.js";
+export * from "./nostr-url.js";
+export * from "./repo-addr.js";
+export * from "./sanitize-relays.js";
+export * from "./validation.js";
